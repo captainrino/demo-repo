93f7278fd94df0198e11071a7e492a358c8d7bb2
Sharan <sharan.kundapur@gmail.com>
2023-03-26 17:52:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-[100vh] text-lg">
+      <div>hello guys lets go?</div>
+    </div>
+  );
+};
+
+export default App;
